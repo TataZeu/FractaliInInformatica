997df49f1f72abca209234224e339ef3d37d3f9c
Razvan Ispas <razvanip@yahoo.com>
2018-05-19 11:29:37 +0300

    Aceasta este baza

---
+++ Baza programului/Baza programului/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Baza_programului
+{
+    partial class Meniu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Koch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Koch
+            // 
+            this.Koch.Location = new System.Drawing.Point(88, 301);
+            this.Koch.Name = "Koch";
+            this.Koch.Size = new System.Drawing.Size(107, 36);
+            this.Koch.TabIndex = 0;
+            this.Koch.Text = "Koch";
+            this.Koch.UseVisualStyleBackColor = true;
+            this.Koch.Click += new System.EventHandler(this.Koch_Click);
+            // 
+            // Meniu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 519);
+            this.Controls.Add(this.Koch);
+            this.Name = "Meniu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Koch;
+    }
+}
+
---
+++ Baza programului/Baza programului/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baza_programului
+{
+    public partial class Panza : Form
+    {
+        public Panza()
+        {
+            InitializeComponent();
+        }
+
+        double r = Math.Sqrt(3) / 6;
+
+        public void fulg(int n, int xa, int ya, int xb, int yb, Graphics gobject, Pen pen)
+        {
+            int xc, yc, xm, ym, xd, yd;
+            if (n > 1)
+            {
+
+                xm = Convert.ToInt32((xa + xb) / 2 + r * (ya - yb));
+                ym = Convert.ToInt32((ya + yb) / 2 + r * (xb - xa));
+                xc = (2 * xa + xb) / 3;
+                yc = (2 * ya + yb) / 3;
+                xd = (xa + 2 * xb) / 3;
+                yd = (ya + 2 * yb) / 3;
+                fulg(n - 1, xa, ya, xc, yc, gobject, pen);
+                fulg(n - 1, xc, yc, xm, ym, gobject, pen);
+                fulg(n - 1, xm, ym, xd, yd, gobject, pen);
+                fulg(n - 1, xd, yd, xb, yb, gobject, pen);
+            }
+            else
+                gobject.DrawLine(pen, xa, ya, xb, yb);
+
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Meniu obiect = new Meniu();
+
+            Graphics gobject = canvas.CreateGraphics();
+            Pen pen = new Pen(Color.Gray, 4);
+            
+            if (Meniu.cod==1)
+            {
+                fulg(13, 300, 10, 100, 356, gobject, pen);
+                fulg(13, 100, 356, 500, 356, gobject, pen);
+                fulg(13, 500, 356, 300, 10, gobject, pen);
+            }
+        }
+    }
+}
